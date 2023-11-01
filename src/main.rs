d1a9d052be86900af8451972c17259be60fff188
Elgius <114131653+Elgius@users.noreply.github.com>
2023-11-01 15:31:00 +0500

    working

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 use headless_chrome::Browser;
+use headless_chrome::protocol::cdp::Page;
+use std::error::Error;
 use std::io::{self, Write};
-// use std::error::Error as StdError;
-use failure::Error;
 
-fn google_search(search: &str) -> Result<(), failure::Error> {
-    // Google search code, work in progress
+fn google_search(search: &str) -> Result<(), Box<dyn Error>> {
+    // Google search code
+    // todo: get the browser to open lol
 
-    let browser = Browser::default().map_err(Error::from_boxed_compat(err))?;
+    let browser = Browser::default()?;
 
-    let tab = browser.new_tab().map_err(Error::from_boxed_compat(err))?;
+    let tab = browser.new_tab()?;
 
-    let url = "https://www.google.co.in/search?q={search}";
-    tab.navigate_to(&url).map_err(Error::from_boxed_compat(err))?;
+    let query = search;
+    println!("this is the query you are making: {}", query);
+
+    let url = format!("https://www.google.co.in/search?q={}", query);
+    tab.navigate_to(&url)?;
+
+    let _jpeg_data = tab.capture_screenshot(
+        Page::CaptureScreenshotFormatOption::Jpeg,
+        None,
+        None, 
+        true)?;
 
     Ok(())
 }
@@ -31,13 +41,12 @@ fn input_data() -> String {
     // cleaning data off whitespaces and making it into a string for safety measure
     let data_cleaner: String = search.trim().to_string();
 
-
     // setting string limits for efficient speeds
     if data_cleaner.len() > 20 {
         println!("please reduce the search size, be concise as much as possible");
         return input_data();
     } else {
-        println!("{}", data_cleaner);
+        // println!("{}", data_cleaner);
         return data_cleaner;
     }
 }
