36fb01fbbd3260a758a47ba33d35f6834c874330
Elgius <114131653+Elgius@users.noreply.github.com>
2023-11-02 09:37:22 +0500

    removed Chrome support cuz Chromium is annoying

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,49 @@
-use headless_chrome::Browser;
 use headless_chrome::protocol::cdp::Page;
+use headless_chrome::{protocol::cdp::Page::CaptureScreenshotFormatOption, Browser, LaunchOptions};
 use std::error::Error;
 use std::io::{self, Write};
+// use std::path::PathBuf;
+// use std::io::Error;
+use std::process::Command;
 
-fn google_search(search: &str) -> Result<(), Box<dyn Error>> {
-    // Google search code
-    // todo: get the browser to open lol
-
-    let browser = Browser::default()?;
-
-    let tab = browser.new_tab()?;
-
-    let query = search;
-    println!("this is the query you are making: {}", query);
-
-    let url = format!("https://www.google.co.in/search?q={}", query);
-    tab.navigate_to(&url)?;
-
-    let _jpeg_data = tab.capture_screenshot(
-        Page::CaptureScreenshotFormatOption::Jpeg,
-        None,
-        None, 
-        true)?;
-
-    Ok(())
+fn mozilla_firefox() {
+    let output = Command::new("firefox")
+        .arg("http://www.example.com")
+        .output()
+        .expect("failed to run the command");
 }
+
+// fn google_search(search: &str) -> Result<(), Box<dyn Error>> {
+//     // Google search code
+//     // todo: get the browser to open lol
+
+//     println!("starting the google search");
+
+//     // let mut options = headless_chrome::LaunchOptions::default();
+//     // options.path = Some(PathBuf::from("C:/Program Files(x86)/Google/Chrome/Application/chrome.exe"));
+
+//     // let browser = Browser::new(options)?;
+
+//     // let browser = Browser::default()?;
+
+//     let options = LaunchOptions::default_builder()
+//         .build()
+//         .expect("could not find the chrome binary!, dont you love proper error handling!");
+
+//     let browser = Browser::new(options)?;
+//     let tab = browser.new_tab()?;
+
+//     let query = search;
+//     println!("this is the query you are making: {}", query);
+
+//     let url = format!("https://www.google.co.in/search?q={}", query);
+//     tab.navigate_to(&url)?;
+
+//     let _jpeg_data =
+//         tab.capture_screenshot(Page::CaptureScreenshotFormatOption::Jpeg, None, None, true)?;
+
+//     Ok(())
+// }
 
 fn input_data() -> String {
     println!("enter what you want to search today?");
@@ -59,10 +78,11 @@ fn info() {
 fn main() {
     // info() to print out all the info when this starts
     // cuz we in Beta and there will be information that needs to be displayed
-    info();
+    // info();
     // function dedicted to get the data from the user on what to search
-    let search_results = input_data();
+    // let search_results = input_data();
 
     // actual function to search for the data
-    google_search(&search_results);
+    //    let google =  google_search(&search_results);
+    mozilla_firefox();
 }
